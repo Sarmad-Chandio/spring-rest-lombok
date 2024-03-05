9358f5bcfaec0ca7fd617efd343d25c3f32c7f9a
sarmad.sikandar <sarmad.sikandar@systemsltd.com>
2024-03-05 16:51:24 +0500

    Spring mvc Rest Service

--- src/main/java/com/systems/springframework/spring6restmvc/controllers/BeerController.java
+++ src/main/java/com/systems/springframework/spring6restmvc/controllers/BeerController.java
@@ -4,17 +4,36 @@ import com.systems.springframework.spring6restmvc.model.Beer;
 import com.systems.springframework.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
+@RequestMapping("api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
-    public Beer getBeerByUuid(UUID id){
-        log.debug("inside getBeerByUuid() from controller ="+ id);
-        return beerService.getBeerByUuid(id);
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Beer> beerList(){
+        return beerService.beerList();
+    }
+    @RequestMapping(value = "/{beerID}", method = RequestMethod.GET)
+    public Beer getBeerByUuid(@PathVariable("beerID") UUID beerID){
+        log.debug("inside getBeerByUuid() from controller ="+ beerID);
+        return beerService.getBeerByUuid(beerID);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity saveBeerObject(@RequestBody Beer beer){
+        beerService.saveBeerObject(beer);
+
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
--- src/main/java/com/systems/springframework/spring6restmvc/service/BeerServiceImpl.java
+++ src/main/java/com/systems/springframework/spring6restmvc/service/BeerServiceImpl.java
@@ -6,16 +6,18 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.*;
+
 @Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
-    @Override
-    public Beer getBeerByUuid(UUID id) {
-        log.debug("Beer id in service was called "+ id);
 
-        return Beer.builder()
-                .id(id)
+    private Map<UUID, Beer> beerMap;
+
+    public BeerServiceImpl(){
+        this.beerMap=new HashMap<>();
+        Beer beer1 = Beer.builder()
+                .id(UUID.randomUUID())
                 .version(1)
                 .beerName("Galaxy Cat")
                 .beerStyle("Flex")
@@ -25,5 +27,63 @@ public class BeerServiceImpl implements BeerService {
                 .createdDate(LocalDateTime.now())
                 .updatedDate(LocalDateTime.now())
                 .build();
+
+        Beer beer2 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Chao Cat")
+                .beerStyle("Flex-Speacial")
+                .upc("1235651 ")
+                .price(new BigDecimal("12.991"))
+                .quantityOnHold(122)
+                .createdDate(LocalDateTime.now())
+                .updatedDate(LocalDateTime.now())
+                .build();
+
+        Beer beer3 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Chao Marinee")
+                .beerStyle("All Shapes")
+                .upc("12356")
+                .price(new BigDecimal("12.99"))
+                .quantityOnHold(122)
+                .createdDate(LocalDateTime.now())
+                .updatedDate(LocalDateTime.now())
+                .build();
+
+        beerMap.put(beer1.getId(), beer1);
+        beerMap.put(beer2.getId(), beer2);
+        beerMap.put(beer3.getId(), beer3);
+    }
+
+    @Override
+    public List<Beer> beerList(){
+        return new ArrayList<>(beerMap.values());
+    }
+    @Override
+    public Beer getBeerByUuid(UUID id) {
+        log.debug("Beer id in service was called "+ id);
+
+        return beerMap.get(id);
+    }
+
+    @Override
+    public Beer saveBeerObject(Beer beer) {
+        //mimicking DB layer implimentations
+        Beer savedBeer= Beer.builder()
+                .id(UUID.randomUUID())
+                .beerName(beer.getBeerName())
+                .createdDate(LocalDateTime.now())
+                .upc(beer.getUpc())
+                .version(beer.getVersion())
+                .price(beer.getPrice())
+                .beerStyle(beer.getBeerStyle())
+                .updatedDate(LocalDateTime.now())
+                .quantityOnHold(beer.getQuantityOnHold())
+                .build();
+
+        beerMap.put(savedBeer.getId(),savedBeer);
+        return savedBeer;
     }
 }
