bebcd3c47b5264be2569d1f4a7ab4b83fa23744d
sarmad.sikandar <sarmad.sikandar@systemsltd.com>
2024-03-07 13:22:39 +0500

    Spring MVC : Mocito Testing

--- src/test/java/com/systems/springframework/spring6restmvc/controllers/BeerControllerTestJUnit5.java
+++ src/test/java/com/systems/springframework/spring6restmvc/controllers/BeerControllerTestJUnit5.java
@@ -1,22 +1,32 @@
 package com.systems.springframework.spring6restmvc.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.systems.springframework.spring6restmvc.model.Beer;
 import com.systems.springframework.spring6restmvc.service.BeerService;
 import com.systems.springframework.spring6restmvc.service.BeerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -28,11 +38,24 @@ class BeerControllerTestJUnit5 {
 
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;//Configuration Done By Spring Boot i-e: Formating data in our scenerio
+
+    @Captor
+    ArgumentCaptor<UUID> uuidArgumentCaptor;
+    @Captor
+    ArgumentCaptor<Beer> beerArgumentCaptor;
 
     @MockBean
     BeerService beerService;
 
-    BeerServiceImpl beerServiceImpl=new BeerServiceImpl();
+    BeerServiceImpl beerServiceImpl;
+
+    @BeforeEach
+    void setUp() {
+        beerServiceImpl=new BeerServiceImpl();
+        uuidArgumentCaptor= ArgumentCaptor.forClass(UUID.class);
+    }
 
     //test case for beerList
     @Test
@@ -46,6 +69,80 @@ class BeerControllerTestJUnit5 {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.length()",is(3)));
 
+    }
+    @Test
+    void testDeleteBeer() throws Exception{
+        Beer beer=beerServiceImpl.beerList().get(0);
+
+        mockMvc.perform(delete("/api/v1/beer/"+ beer.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        verify(beerService).deleteByUuid(uuidArgumentCaptor.capture());
+
+        assertThat(beer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
+    }
+    @Test
+    void testPatchBeer() throws Exception{
+        Beer beer=beerServiceImpl.beerList().get(0);
+
+        Map<String, Object> beerMap=new HashMap<>();
+        beerMap.put("beerName","New Name");
+
+        mockMvc.perform(patch("/api/v1/beer/"+beer.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beerMap)))
+                .andExpect(status().isNoContent());
+
+        verify(beerService).patchByUuid(uuidArgumentCaptor.capture(),beerArgumentCaptor.capture());
+
+        assertThat(beer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
+        assertThat(beerMap.get("beerName")).isEqualTo(beerArgumentCaptor.getValue().getBeerName());
+
+    }
+
+
+    @Test
+    void testUpdateByUuid() throws Exception{
+        Beer beer= beerServiceImpl.beerList().get(0);
+
+        mockMvc.perform(put("/api/v1/beer/"+ beer.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beer)))
+                .andExpect(status().isNoContent());
+        // we can also use ArgumentCaptor for "any" argument
+
+        verify(beerService).updateByUuid(any(UUID.class), any(Beer.class));
+    }
+
+    @Test
+    void testCreatedNewBeer() throws Exception {
+        /*
+        ObjectMapper objectMapper= new ObjectMapper();
+        // Jackson has modules, date and time will be handles by jackson lib
+        // object Mapper could be provided by Spring Framework
+        objectMapper.findAndRegisterModules();
+       */
+        Beer beer=beerServiceImpl.beerList().get(0);
+        //beer object should not have version as well as id
+        beer.setVersion(null);
+        beer.setId(null);
+
+        // perform mockito : returning 1st element form beeList method and passing to sAve beer service
+        given(beerService.saveBeerObject(any(Beer.class))).willReturn(beerServiceImpl.beerList().get(1));
+
+        //perform mocking with the help of mvc
+        mockMvc.perform(post("/api/v1/beer") //action POST
+                .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beer))) //getting data from response object with the help of object mapper
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
+
+
+//        System.out.println(objectMapper.writeValueAsString(beer));
     }
 
 
